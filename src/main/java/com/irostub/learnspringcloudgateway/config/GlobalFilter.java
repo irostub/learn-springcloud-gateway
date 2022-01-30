2519050ccf3d3a8b52921ed95f2f9bad233d135b
irostub <irostub@gmail.com>
2022-01-31 01:17:57 +0900

    Implement global filter

---
+++ src/main/java/com/irostub/learnspringcloudgateway/config/GlobalFilter.java
@@ -0,0 +1,49 @@
+package com.irostub.learnspringcloudgateway.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) ->{
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("global filter base message: {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){
+                log.info("global filter pre logger, request id: {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if(config.isPostLogger()){
+                    log.info("global filter post logger, response code: {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Getter
+    @Setter
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
